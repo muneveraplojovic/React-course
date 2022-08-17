f4e036423e7cb26c7599b2df6ea75183afcfeb86
Munevera Plojovic <plojovicm3@gmail.com>
2022-08-17 20:53:53 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import './App.css';
-import React from 'react';
+import React from 'react'
+import Card from "./components/Card"
+import MyComponent from "./components/MyComponent"
 
-function App() {
-  return (
-    <div className="todo_app">
-      <h1>to do</h1>
+export default function App(){
+
+  return(
+    <UserContext.Provider value={{myState, setMyState}}
+    <div>
+      <h1>Maja</h1>
     </div>
-  );
+    </UserContext.Provider>
+  )
 }
 
-export default App;
+// export default function App() {
+  // const [myState, setMyState] = useState("Hello world")
+  // const [fleg, setFleg]=useState(false)
+  // const myFunc=useCallback(()=>{
+  //   console.log(myState);
+//   // }, [flag])
+//   return (
+//     <div>
+//       {/* <h1>{myState}</h1>
+//       <button onClick={()=>myFunc()}>Invoke useCallBack</button>
+//       <button onClick={()=>setMyState(myState + "A")}>chagde my state</button>
+//       <button onClick={()=>setFleg(!fleg)}>Invoke useCallBack</button> */}
+
+//     </div>
+//   )
+// }
+
+// //     <div>
+// //       {/* <Routes>
+//         <Route path={"/"} element={<h1>Welcome</h1>}></Route>
+//         <Route path={"/drop-down"} element={<h1>Dropdown</h1>}></Route>
+//         <Route path={"/google"} element={<h1>Google</h1>}></Route>
+//         <Route path={"/text"} element={<h1>Text</h1>}></Route>
+//         </Routes>
+//     <div>
+//       <Link to={"/"}>
+//         <h1>Welcome</h1>
+//       </Link>
+//       <Link to={"/counter"}>
+//         <h1>Counter</h1>
+//       </Link>
+//       <Link to={"/dropdown"}>
+//         <h1>Drop-down</h1>
+//       </Link>
+//       <Link to={"/list"}>
+//         <h1>To do list</h1>
+//       </Link>
+//     </div> */}
+
+
+//     </div>
