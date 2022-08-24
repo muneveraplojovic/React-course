5ffdf52abf6aeeaa0ef6019b7b860852ee19271b
Munevera Plojovic <plojovicm3@gmail.com>
2022-08-24 20:03:24 +0200

    new

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import MyComponent from "./components/MyComponent"
 
 export default function App(){
 
-  return(
-    <UserContext.Provider value={{myState, setMyState}}
-    <div>
-      <h1>Maja</h1>
-    </div>
-    </UserContext.Provider>
-  )
+  return <h1>Maja</h1>
 }
 
 export default function App() {
@@ -21,36 +15,31 @@ export default function App() {
   // }, [flag])
   return (
     <div>
-      {/* <h1>{myState}</h1>
-      <button onClick={()=>myFunc()}>Invoke useCallBack</button>
-      <button onClick={()=>setMyState(myState + "A")}>chagde my state</button>
-      <button onClick={()=>setFleg(!fleg)}>Invoke useCallBack</button> */}
-
-    </div>
+/    </div>
   )
 }
 
-// //     <div>
-// //       {/* <Routes>
-//         <Route path={"/"} element={<h1>Welcome</h1>}></Route>
-//         <Route path={"/drop-down"} element={<h1>Dropdown</h1>}></Route>
-//         <Route path={"/google"} element={<h1>Google</h1>}></Route>
-//         <Route path={"/text"} element={<h1>Text</h1>}></Route>
-//         </Routes>
 //     <div>
-//       <Link to={"/"}>
-//         <h1>Welcome</h1>
-//       </Link>
-//       <Link to={"/counter"}>
-//         <h1>Counter</h1>
-//       </Link>
-//       <Link to={"/dropdown"}>
-//         <h1>Drop-down</h1>
-//       </Link>
-//       <Link to={"/list"}>
-//         <h1>To do list</h1>
-//       </Link>
-//     </div> */}
+//       {/* <Routes>
+        <Route path={"/"} element={<h1>Welcome</h1>}></Route>
+        <Route path={"/drop-down"} element={<h1>Dropdown</h1>}></Route>
+        <Route path={"/google"} element={<h1>Google</h1>}></Route>
+        <Route path={"/text"} element={<h1>Text</h1>}></Route>
+        </Routes>
+    <div>
+      <Link to={"/"}>
+        <h1>Welcome</h1>
+      </Link>
+      <Link to={"/counter"}>
+        <h1>Counter</h1>
+      </Link>
+      <Link to={"/dropdown"}>
+        <h1>Drop-down</h1>
+      </Link>
+      <Link to={"/list"}>
+        <h1>To do list</h1>
+      </Link>
+    </div> */}
 
 
-//     </div>
+    </div>
